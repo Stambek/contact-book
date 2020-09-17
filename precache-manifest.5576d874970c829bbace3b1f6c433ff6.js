ae6f805657001357e3b0f220e9c8c37a99a53c19
Stambek <Stambekm@gmail.com>
2020-09-17 23:57:13 +0600

    Updates

---
+++ precache-manifest.5576d874970c829bbace3b1f6c433ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28cd70ace9d4a3f0c57b93707775ae6",
+    "url": "/contact-book/index.html"
+  },
+  {
+    "revision": "136aae0c29fa29cfebb4",
+    "url": "/contact-book/static/css/main.ff63c00c.chunk.css"
+  },
+  {
+    "revision": "0f1d034221d909df6a5a",
+    "url": "/contact-book/static/js/2.01c0ce9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact-book/static/js/2.01c0ce9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136aae0c29fa29cfebb4",
+    "url": "/contact-book/static/js/main.935bf52b.chunk.js"
+  },
+  {
+    "revision": "5d0e70e2a3c3b8096c83",
+    "url": "/contact-book/static/js/runtime-main.355b84d7.js"
+  }
+]);
